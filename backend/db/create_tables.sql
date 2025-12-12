3a6d41eefb0d68d87f79bea6d08dd141d9418e3b
Michael <michaelyu713705@gmail.com>
2025-12-12 19:16:34 +0000

    fix create tables

--- backend/db/create_tables.sql
+++ backend/db/create_tables.sql
@@ -1,8 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "vector";
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    public_id TEXT GENERATED ALWAYS AS ('usr_' || id::TEXT) STORED UNIQUE
     email VARCHAR(255),
     name VARCHAR(255),
     image TEXT,
@@ -11,23 +10,24 @@ CREATE TABLE users (
 
 CREATE TABLE activations(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sample_id TEXT,
+    dataset_id TEXT,
     "tokens" TEXT[],
     "values" DOUBLE PRECISION[] DEFAULT ARRAY[]::DOUBLE PRECISION[],
     "index" TEXT NOT NULL,
     "layer" TEXT NOT NULL,
     "model_id" TEXT NOT NULL,
-    "feature_id" TEXT, 
+    "feature_id" UUID, 
     "max_value" DOUBLE PRECISION ,
     "max_value_token_index" INTEGER ,
     "min_value" DOUBLE PRECISION ,
-    "created_by" TEXT,
+    "created_by" UUID,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW()
 );
 
 
 CREATE TABLE "features" (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    public_id TEXT GENERATED ALWAYS AS ('ftr_' || id::TEXT) STORED UNIQUE
     "model_id" TEXT NOT NULL,
     "layer" TEXT NOT NULL,
     "index" TEXT NOT NULL,
@@ -37,23 +37,23 @@ CREATE TABLE "features" (
     "pos_str" TEXT[] DEFAULT ARRAY[]::TEXT[],
     "pos_values" DOUBLE PRECISION[] DEFAULT ARRAY[]::DOUBLE PRECISION[],
     "frac_nonzero" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "created_by" TEXT,
+    "created_by" UUID,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW()
 );
 
 
 CREATE TABLE "explanations" (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    public_id TEXT GENERATED ALWAYS AS ('exp_' || id::TEXT) STORED UNIQUE,
     "model_id" TEXT NOT NULL,
     "layer" TEXT NOT NULL,
     "index" TEXT NOT NULL,
+    feature_id UUID,
     "embedding" vector(256),
     "description" TEXT NOT NULL,
-    "created_by" TEXT,
     "notes" TEXT,
     "type_name" TEXT,
     "explanation_model" TEXT,
+    created_by UUID,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW()
 );
