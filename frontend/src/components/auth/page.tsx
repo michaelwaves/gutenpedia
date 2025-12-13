256c00f7ae96edcc8893ce86673cb51267a30bfc
Michael <michaelyu713705@gmail.com>
2025-12-13 02:49:05 +0000

    add vite and tailwindcss

--- frontend/src/components/auth/page.tsx
+++ frontend/src/components/auth/page.tsx
@@ -1,12 +1,15 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from 'jwt-decode'
+import { useNavigate } from "react-router-dom";
 function LoginPage() {
+    const navigate = useNavigate()
     return (
         <div>
             Login
             <GoogleLogin onSuccess={(res) => {
                 console.log(res)
                 console.log(jwtDecode(res.credential!))
+                navigate('/d')
             }}
                 onError={() => alert("Login Failed!")}
             />
--- frontend/src/components/dashboard/page.tsx
+++ frontend/src/components/dashboard/page.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function DashboardPage() {
+    const navigate = useNavigate()
+    function handleLogout() {
+
+    }
     return (
         <div>
             Dashboard
+
         </div>
     );
 }
