702df5cd26db51895059a44241c43dfff858985c
Michael <michaelyu713705@gmail.com>
2025-12-14 18:13:54 +0000

    add kysely

--- next/lib/db.ts
+++ next/lib/db.ts
@@ -1,5 +1,6 @@
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
+import { DB } from "./schema";
 
 if (!process.env.DATABASE_URL) {
     throw new Error("Must set env var DATABASE_URL ")
@@ -11,6 +12,6 @@ const dialect = new PostgresDialect({
 })
 
 
-export const db = new Kysely<>({
+export const db = new Kysely<DB>({
     dialect
 })
